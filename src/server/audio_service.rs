518f56f52b506fa29f06bed6b28ee48d4634cac7
ttcms <1454176872@qq.com>
2025-07-18 23:54:11 +0800

    同时采集外放声音和麦克风声音

--- src/server/audio_service.rs
+++ src/server/audio_service.rs
@@ -19,7 +19,6 @@ use magnum_opus::{Application::*, Channels::*, Encoder};
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::collections::VecDeque;
 use std::pin::Pin;
-use futures::Stream;
 
 pub const NAME: &'static str = "audio";
 pub const AUDIO_DATA_SIZE_U8: usize = 960 * 4; // 10ms in 48000 stereo
